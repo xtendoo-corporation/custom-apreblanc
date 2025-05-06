66c475d1cd7f80c639080a3965de550c1d1566c9
PepeDev26 <pepeaguilaralanis@hotmail.com>
2025-05-06 09:38:25 +0200

    Now featuring: Polymorphism

--- apreblanc_probabilty/models/sale_order.py
+++ apreblanc_probabilty/models/sale_order.py
@@ -8,19 +8,3 @@ class SaleOrder(models.Model):
         default=0.0,
         help='Probabilidad de confirmación del pedido',
     )
-# Añadir este campo al modelo SaleOrder en models/sale_order.py
-    margin_percent = fields.Float(
-        string='% Margen',
-        compute='_compute_margin_percent',
-        store=True,
-        digits=(5, 2),
-        help='Porcentaje de margen del pedido',
-    )
-
-    @api.depends('amount_untaxed', 'margin')
-    def _compute_margin_percent(self):
-        for order in self:
-            if order.amount_untaxed != 0:
-                order.margin_percent = (order.margin / order.amount_untaxed) * 100
-            else:
-                order.margin_percent = 0.0
