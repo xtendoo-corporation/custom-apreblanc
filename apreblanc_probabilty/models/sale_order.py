f789da6c2f8ee1a28e71a39e4c3017ce7fb1d44d
PepeDev26 <pepeaguilaralanis@hotmail.com>
2025-05-07 09:08:52 +0200

    Translations and pivot

--- apreblanc_probabilty/models/sale_order.py
+++ apreblanc_probabilty/models/sale_order.py
@@ -8,3 +8,15 @@ class SaleOrder(models.Model):
         default=0.0,
         help='Probability of closing the sale order',
     )
+
+    margin_percent = fields.Float(
+        string='% Margin',
+        compute='_compute_margin_percent',
+        store=True,
+        digits=(5, 2),
+    )
+
+    @api.depends('margin', 'amount_untaxed')
+    def _compute_margin_percent(self):
+        for order in self:
+            order.margin_percent = order.amount_untaxed and (order.margin / order.amount_untaxed) * 100 or 0.0
