a52140d28d2716b57f0461eb360a65ec2da7be04
dani <dani.xtendoo@gmail.com>
2025-07-24 14:35:10 +0200

    sync

--- xtendoo_onedrive_connector/models/onedrive_document.py
+++ xtendoo_onedrive_connector/models/onedrive_document.py
@@ -65,7 +65,7 @@ class OneDriveDocument(models.Model):
             }
 
     def action_download_file(self):
-        # Lógica para descargar un archivo desde OneDrive
+        # Lógica para descargar un archivo desde OneDriveaaa
         return True
 
     def action_upload_file(self):
