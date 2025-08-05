ec552adde4174f1e15a7038d28db67daa032581d
dani <dani.xtendoo@gmail.com>
2025-08-05 10:53:44 +0200

    sincroniza cuenta profesional

--- xtendoo_onedrive_connector/models/onedrive_settings.py
+++ xtendoo_onedrive_connector/models/onedrive_settings.py
@@ -11,6 +11,7 @@ class OneDriveSettings(models.Model):
     onedrive_tenant_id = fields.Char('Tenant ID', required=True)
     onedrive_redirect_uri = fields.Char('Redirect URI', required=True)
     onedrive_refresh_token = fields.Char('Refresh Token')
+    onedrive_sync_folder = fields.Char('Carpeta a Sincronizar', help='Ruta o ID de la carpeta de OneDrive que quieres sincronizar. Déjalo en blanco para sincronizar la carpeta raíz.')
 
     def get_auth_url(self):
         # Usar tenant específico en lugar del endpoint común
