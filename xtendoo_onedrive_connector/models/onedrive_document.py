b09b72cd32e258bdd4757cb3c0e99073cf140954
dani <dani.xtendoo@gmail.com>
2025-08-05 11:13:52 +0200

    añadido parametro folder_to_sync, que sera la que sincronice, ademas de crearla si no existe al realziar la prueba de conexion

--- xtendoo_onedrive_connector/models/onedrive_document.py
+++ xtendoo_onedrive_connector/models/onedrive_document.py
@@ -53,7 +53,7 @@ class OneDriveDocument(models.Model):
         Método llamado desde la interfaz para sincronizar con OneDrive
         """
         try:
-            # Usar el servicio mejorado de OneDrive
+            # Usar el servicio mejoradoo de OneDrive
             service = self.env['onedrive.service']
             result = service.sync()
 
